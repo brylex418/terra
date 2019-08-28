195086c238f4761e974fd8dd468da7bdd3bdda74
brylex <brylex@amazon.com>
2019-08-28 11:56:36 -0700

    housekeeping

--- userdata.sh
+++
@@ -1,6 +0,0 @@
-    #!/bin/bash
-    sudo apt udpate -y
-    sudo apt install apache2 -y
-    echo "<h1>"Deployed Via Terraform"</h1>" | sudo tee /var/www/html/index.html
-    sudo systemctl start apache2
-    sudo systemctl enable apache2
